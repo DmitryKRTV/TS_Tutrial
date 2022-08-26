9dfb28a41e56f7f3943c796707a3e33d7e1f11ec
DmitryKRTV <dmitrykorotaev.job@gmail.com>
2022-08-26 17:52:16 +0300

    Lesson 22. Added to useEffect stories th new story about setTimeout and setInterval

--- src/stories/useEffect/UseEffect.stories.tsx
+++ src/stories/useEffect/UseEffect.stories.tsx
@@ -43,3 +43,51 @@ export const UseEffectDemo = () => {
     </>
 }
 
+export const SetTimeoutEmaple = () => {
+    console.log("UseEffectDemo Component")
+    const [seconds, setSeconds] = useState(0);
+    const [minutes, setMinutes] = useState(0);
+    const [hours, setHors] = useState(0);
+
+    // useEffect(()=> {
+    //     console.log("useEffect every render")
+    //     // setTimeout(() => {
+    //     //     document.title = counter.toString()
+    //     // }, 1000)
+    //     //api.getUsers().then("")
+    //     //setInterval
+    //     //indexedDB
+    //     //document.getElementID
+    // },)
+    useEffect(()=> {
+        console.log("only firs render (componentsDidMount)")
+
+        setInterval(() => {
+            let date = new Date();
+            setHors(() => date.getHours())
+            setMinutes(() => date.getMinutes())
+            setSeconds(() => date.getSeconds())
+        }, 1000)
+        //indexedDB
+        //document.getElementID
+    },[])
+    // useEffect(()=> {
+    //     console.log("Will be launched 1 render and when counter will be changed")
+    //     // setInterval(() => {
+    //     //     setCounter(counter + 1)
+    //     // }, 1000)
+    //     // setTimeout(() => {
+    //     //     document.title = counter.toString()
+    //     // }, 1000)
+    //     //api.getUsers().then("")
+    //     //setInterval
+    //     //indexedDB
+    //     //document.getElementID
+    // },[counter])
+
+    return <>
+        {/*<button onClick={() => setCounter(counter + 1)}></button>*/}
+        <div>{hours} : {minutes} : {seconds}</div>
+    </>
+}
+
